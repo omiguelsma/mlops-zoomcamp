c2fb99cbf0077aefc30f59adc273f4bfa785a40e
omiguelsma <omiguelsma@gmail.com>
2025-05-19 18:02:52 -0300

    Modif - def read_dataframe

--- src/read_dataframe.py
+++ src/read_dataframe.py
@@ -9,13 +9,4 @@ def read_dataframe(filename):
 
     elif filename.endswith('.parquet'):
         df = pd.read_parquet(filename)
-
-    '''df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
-    df.duration = df.duration.apply(lambda td: td.total_seconds() / 60)
-
-    df = df[(df.duration >= 1) & (df.duration <= 60)]
-
-    categorical = ['PULocationID', 'DOLocationID']
-    df[categorical] = df[categorical].astype(str)
-    '''
     return df
